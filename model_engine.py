f353f9bcf2d0f32260abce0cc61f8ff4b7895bbd
Maryam Syafiah <syafiahmaryam@gmail.com>
2026-04-15 22:32:50 -0400

    improved to accomodate BOW feature

--- model_engine.py
+++ model_engine.py
@@ -5,7 +5,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import cross_val_score, KFold
 
 def run_logistic_regression(book_data):
-    """Performs predictive modeling and evaluates accuracy."""
+    """Trains a classifier on a balanced database (1:1 ratio)."""
     success = book_data[book_data['label'] == 1]
     other = book_data[book_data['label'] == 0]
     
@@ -15,9 +15,10 @@ def run_logistic_regression(book_data):
         other.sample(n=sample_size, random_state=42)
     ]).sample(frac=1).reset_index(drop=True)
 
-    features = ['sentiment', 'review/score', 'review_count']
+    features = [col for col in df_balanced.columns if col not in ['clean_name', 'label']]
     X = df_balanced[features].values
     y = df_balanced['label'].values
+    # Standardization is imperative for Logistic Regression performance [cite: 232, 336]
     X_scaled = StandardScaler().fit_transform(X)
 
     model = LogisticRegression()
